2551073b5f426de1b1e9d39c600082ded7f02020
gabrielsqsf <gabrielsqsf@gmail.com>
2015-10-05 08:52:45 -0300

    Issue: MOSC-243 Subject: Modificações no IVS para lançamento da Versão com RM Detail: Acertando alinhamento vertical das imagens.

--- web/destaques/destaquesView2.php
+++ web/destaques/destaquesView2.php
@@ -100,7 +100,7 @@
 
         <div id="conteudo_atlas">
         	<div>
-                <img src="./assets/img/icons/favicon.png" />
+                <img src="./assets/img/icons/favicon.png" style="vertical-align: none;" />
         	    <a style="display: inline-block;font-size: 16px;margin-bottom: 16px;" href="http://g1.globo.com/distrito-federal/noticia/2015/09/vulnerabilidade-social-cai-mas-ainda-e-alta-no-norte-e-no-nordeste-diz-ipea.html">
                     <span style="float:left;">Vulnerabilidade social cai, mas ainda é alta no Norte e no Nordeste, diz Ipea</span>
                     <span style="float:left; clear:left; color:#AAA; font-size:12px;">terça-feira, 1 de setembro de 2015 - 12:16:18</span>
@@ -108,14 +108,14 @@
                 </a>
             </div>
             <div>
-                <img src="./assets/img/icons/favicon.png" />
+                <img src="./assets/img/icons/favicon.png" style="vertical-align: none;" />
                 <a style="display: inline-block;font-size: 16px;margin-bottom: 16px;" href="http://g1.globo.com/al/alagoas/noticia/2015/09/al-e-o-estado-do-nordeste-com-maior-vulnerabilidade-social-revela-ipea.html">
                     <span style="float:left;">AL é o estado do Nordeste com maior vulnerabilidade social, revela Ipea</span>
                     <span style="float:left; clear:left; color:#AAA; font-size:12px;">terça-feira, 1 de setembro de 2015 - 13:06:08</span>
                     <span style="clear:both;"></span>
                 </a>
             <div>
-                <img src="./assets/img/icons/favicon.png" />
+                <img src="./assets/img/icons/favicon.png" style="vertical-align: none;" />
                 <a style="display: inline-block;font-size: 16px;margin-bottom: 16px;" style="display:" href="http://diariocatarinense.clicrbs.com.br/sc/economia/noticia/2015/09/vulnerabilidade-social-o-que-significa-o-indice-do-ipea-em-que-sc-se-destaca-4838033.html">
                     <span style="float:left;">Vulnerabilidade social: o que significa o índice do Ipea em que SC se destaca</span>
                     <span style="float:left; clear:left; color:#AAA; font-size:12px;">terça-feira, 1 de setembro de 2015 - 13:23:00</span>
@@ -123,7 +123,7 @@
                 </a>
             </div>
             <div>
-                <img src="./assets/img/icons/favicon.png" />
+                <img src="./assets/img/icons/favicon.png" style="vertical-align: none;" />
 			    <a style="display: inline-block;font-size: 16px;margin-bottom: 16px;" href="http://g1.globo.com/pa/para/noticia/2015/09/riquezas-do-minerio-nao-chegam-aos-moradores-de-maraba-diz-ipea.html">
                     <span style="float:left;">Riquezas do minério não chegam aos moradores de Marabá, diz Ipea</span>
                     <span style="float:left; clear:left; color:#AAA; font-size:12px;">terça-feira, 8 de setembro de 2015 - 17:24:14</span>         
