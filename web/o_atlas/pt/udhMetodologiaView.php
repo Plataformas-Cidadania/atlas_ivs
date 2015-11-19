0bff6b990f5968a35a8890f5634794238feebc94
gabrielsqsf <gabrielsqsf@gmail.com>
2015-11-19 15:10:11 -0200

    Issue: IVS-12 Subject: Incluir texto sobre a metodologia das UDHs na aba METODOLOGIA Detail: Correções.

--- web/o_atlas/pt/udhMetodologiaView.php
+++ web/o_atlas/pt/udhMetodologiaView.php
@@ -6,13 +6,13 @@
     <div>
     	<h2>O que é uma Unidade de Desenvolvimento Humano (UDH)</h2>
     	<p>As UDHs são recortes territoriais localizados dentro das áreas metropolitanas que podem ser uma parte de um bairro, um bairro completo ou, em alguns casos, até um município pequeno. A definição dos limites das UDHs é entendida a partir da homogeneidade socioeconômica das mesmas, formadas com base nos setores censitários do IBGE.</p>
-    	<h2>Como é feita a obtenção de dados das UDHs?</h2>
+    	<h2>Como é feita a obtenção de dados das UDHs</h2>
 		<p>Apesar de coletado em nível de domicílios e de pessoas, os dados dos Censos somente são liberados agregados, para evitar a exposição de informações personalizadas. No caso das informações constantes no questionário aplicado no universo dos domicílios, os dados estão disponíveis para os setores censitários. Já no caso do questionário da amostra, do qual o Atlas retira a maior parte de seus indicadores, os dados estão disponíveis apenas para as áreas de ponderação.</p>
 		<p>Para obter o acesso aos dados do questionário da amostra para recortes espaciais diferentes daqueles correspondentes às áreas de ponderação, os usuários devem submeter um projeto com a proposta da nova agregação para avaliação do IBGE, observando as exigências de confiabilidade estatística e obedecendo a critérios que serão rigorosamente avaliados por um comitê técnico. Entre os parâmetros avaliados pelo comitê, destaca-se, em especial, a exigência de que as áreas criadas devem ter, pelo menos, 400 domicílios particulares permanentes amostrados.</p>
 		<p>Uma vez aprovado o projeto com a criação de novos recortes espaciais para extração de dados, os usuários utilizam uma sala especial disponibilizada pelo IBGE, a chamada “sala de sigilo”, em que têm acesso aos microdados dos Censos segundo sua agregação espacial. Os resultados agregados obtidos a partir dos microdados passam ainda pela avaliação de consistência pelo IBGE antes de serem finalmente liberados ao usuário.</p>
 		<p>O processo acima descrito descreve os procedimentos observados pela equipe do projeto para obtenção dos indicadores do Atlas do Desenvolvimento Humano nas Regiões Metropolitanas, no caso dos recortes espaciais chamados de Unidades de Desenvolvimento Humano (UDH).</p>
 		
-		<h2>O que é um setor censitário</h2>
+		<h2>Setor censitário</h2>
 		<p>O setor censitário é constituído de áreas contíguas, delimitadas para atender aos parâmetros da coleta e para controle cadastral. Situa-se em um único quadro urbano ou rural e o número de domicílios nele contidos e sua dimensão territorial são definidos de forma a permitir o levantamento das informações por um único recenseador. É definido em função da rota do recenseador, obedecendo a barreiras físicas e à lógica dos logradouros. Sua configuração assemelha-se às rotas dos leituristas de relógio de água, de energia elétrica, de endereçamento postal ou de coleta de lixo (normalmente quarteirões em centros urbanos, prédios muito grandes, etc). Com isso, tende a ser mais homogêneo nas áreas de maior densidade populacional e menos nas mais rarefeitas.</p>
 		
 		<h2>O que é uma área de ponderação</h2>
@@ -20,27 +20,27 @@
 		
 		
 		
-		<h2>Como foram nomeadas as UDH</h2>
+		<h2>Nomeação das UDH</h2>
 		<p>Os parceiros estaduais do Atlas contribuíram com a nomeação das UDHs, conforme sua configuração no espaço urbano dos municípios metropolitanos.</p>
 		<p>Quando uma única UDH corresponde integralmente (ou quase integralmente) a um bairro/recorte territorial conhecido pela população, o nome da UDH corresponde ao nome deste bairro ou recorte territorial.Quando o bairro ou recorte territorial possui um apelido ou nome popularmente conhecido, este pode ter sido anotado entre parênteses (bastante comum no caso de vilas, aglomerados, favelas e assentamentos identificados como subnormais). Como a consulta ao Atlas poderá ser feita pelo nome da UDH a partir de qualquer parte deste nome, vale a pena colocar esses “apelidos”, pois eles poderão facilitar a consulta ao Atlas.</p>
-		<p>Exemplos: Milionários - Lindéia</p>
+		<p><em style="margin-left: 3em;">Exemplos: Milionários - Lindéia</em></p>
 		<p>Quando a junção de dois ou mais bairros/recortes territoriais conhecidos correspondem a uma única UDH, o nome da UDH faz referência a todos esses nomes.</p>
-		<p>Exemplos: Lourdes / Santo Agostinho</p>
+		<p><em style="margin-left: 3em;">Exemplos: Lourdes / Santo Agostinho</em></p>
 		<p>Lourdes e Santo Agostinho são bairros contíguos, homogêneos em termos socioeconômicos e, juntos, formam uma única UDH. O nome da UDH inclui a referência aos dois bairros, e, ao fazer a consulta à Plataforma, você irá encontrar a UDH correspondente a esses bairros ao digitar o nome de qualquer um deles.</p>
 		<p>Quando duas UDHs diferentes fazem parte de um mesmo bairro/recorte territorial, o nome do bairro/recorte territorial se encontra seguido do sinal de dois pontos, seguido dos nomes de referência que ajudaram a distinguir uma UDH de outra.</p>
 		<p>Desta forma, há várias UDHs com o mesmo nome de bairro/recorte territorial, mas após os dois pontos (:) as informações complementares facilitam o usuário na identificação de cada UDH, de cada parte do bairro/recorte territorial em questão.</p>
-		<p>Exemplos: Sagrada Família : Horto</p>
-		<p>Sagrada Família : Pátio da Estação Ferroviária</p>
+		<p><em style="margin-left: 3em;">Exemplos: Sagrada Família : Horto</em></p>
+		<p><em style="margin-left: 3em;">Sagrada Família : Pátio da Estação Ferroviária</em></p>
 		<p>Caso a referência complementar ao nome do bairro/recorte territorial envolva mais de uma referência, todos os nomes podem ser utilizados.</p>
-		<p>Exemplos: Hipercentro : Imprensa Oficial / Pça. da Estação / Rodoviária</p>
+		<p><em style="margin-left: 3em;">Exemplos: Hipercentro : Imprensa Oficial / Pça. da Estação / Rodoviária</em></p>
 		
-		<h2>Quais são os critérios para a divisão das UDHs</h2>
+		<h2>Os critérios para a divisão das UDHs</h2>
 		<p>As UDHs foram delineadas buscando gerar áreas mais homogêneas, do ponto de vista das condições socioeconômicas, do que as áreas de ponderação do IBGE. Ou seja, elas são construídas com o objetivo de melhor captar a diversidade de situações relacionadas com o desenvolvimento humano que ocorre no interior dos espaços intrametropolitanos, notadamente em seus grandes municípios, para desvendar o que é escondido pelas médias municipais agregadas, como apresentadas no Atlas do Desenvolvimento Humano nos Municípios.</p>
 		<p>Enquanto a lógica das áreas de ponderação do IBGE atendem a quesitos técnicos relacionados ao processo de coleta e amostragem, as UDHs estão voltadas para a análise espacial das Regiões Metropolitanas (RM) por meio de recortes espaciais de maior homogeneidade socioeconômica, com o objetivo de retratar as desigualdades intrametropolitanas de forma mais contundente.</p>
 		<p>Para se propor esses novos recortes espaciais para a análise e aprovação do IBGE, não foi utilizado nenhum modelo econométrico que pudesse ser aplicado para gerar a conformação das UDHs em todas as RMs do país. As características da vida urbano/metropolitana levam a conformações socioespaciais muito diversas e a homogeneidade obtida por meio da variável A ou B não necessariamente seria igual (ou delinearia a mesma área) se medida por uma segunda variável, considerando-se, ainda, que os próprios setores censitários já trazem consigo algum nível de heterogeneidade. E cada lugar tem a sua morfologia e sua história de ocupação do espaço urbano.</p>
 		<p>Assim, para além das possibilidades associadas à análise das variáveis disponíveis para todos os setores censitários (que poderia ser contemplada por algum modelo econométrico), diversas situações que alteram os dados dos setores censitários (tal como a existência de um único condomínio vertical que altera a média dos dados de um setor censitário) e características da ocupação urbana (tais como idade e perfil dos assentamentos), podem não ser captadas, por exemplo, pela variável renda, e podem interferir nos indicadores sociodemográficos das UDHs. Isto fez com que fosse necessário construir a proposta das UDHs em cada RM de modo “customizado”, atentando-se para as especificidades de cada espaço metropolitano considerado.</p>
 		
-		<h2>Como é feita a divisão regional</h2>
+		<h2>A divisão regional</h2>
 		<p>Na construção das UDHs que aparecem no Atlas e na eventual agregação de UDHs para fins de extração dos dados da amostra, nos municípios em que havia recortes regionais compatíveis com a malha dos setores censitários do Censo Demográfico 2010, esses recortes foram respeitados, de modo que os indicadores estão disponíveis para níveis territoriais (regiões administrativas, regionais, distritos, subprefeituras, etc.) intermediários, inframunicipais, que são agregações das UDHs que compõem essas escalas.</p>
 		    	
     </div>
